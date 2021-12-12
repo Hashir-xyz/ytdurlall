762a5cae38df3c3853e7b5ac5073905ca35b42a0
Hashir-xyz <93175588+Hashir-xyz@users.noreply.github.com>
2021-12-12 11:09:53 +0530

    Update forcesub.py

--- helper_funcs/forcesub.py
+++ helper_funcs/forcesub.py
@@ -12,7 +12,7 @@ async def ForceSub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Son, You are Banned to use me. Contact my [Support Group](https://t.me/TeleRoid14).",
+                text="Sorry Son, You are Banned to use me. Contact my [Support Group](https://t.me/ytdsupport).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -20,11 +20,11 @@ async def ForceSub(bot: Client, cmd: Message):
     except UserNotParticipant:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!\n\nAnd Still If Bot AskS For Joining Updates Channel then Join @MoviesFlixers_DL.",
+            text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!\n\nAnd Still If Bot AskS For Joining Updates Channel then Join @ytdofficial.",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🤖 Join Updates Channel", url="https://t.me/TeleRoidGroup")
+                        InlineKeyboardButton("🤖 Join Updates Channel", url="https://t.me/ytdofficial")
                     ]
                 ]
             ),
